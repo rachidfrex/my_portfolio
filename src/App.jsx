60c2e3d332f9e61522a37d2ea8ee6e8cb04da0c4
rachidfrex <2003043000250@ofppt-edu.ma>
2024-05-09 17:39:58 +0100

    Refactor App and Header components for improved styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Content from './components/content'
 function App() {
 
   return (
-    <div className=' w-full m-0 h-fit'>
+    <div className='    w-full m-0 h-fit'>
       <Nav/>
       <Header/>
       <Content/>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,9 +5,9 @@ function header() {
     <div className='flex flex-col gap-5 items-center justify-center mt-40 '>
     {/*   */}
     <h1
-    className='text-8xl font-semibold   font-rubik bg-clip-text text-transparent bg-gradient-to-b  from-indigo-600 from-15%  via-indigo-300 to-indigo-600 '
+    className='text-8xl font-semibold   font-rubik bg-clip-text text-transparent bg-gradient-to-b  from-indigo-700 from-15%  via-indigo-300 to-indigo-700 '
     >
-        Hi , I'm Frex
+        Hi , I'm Rachid
     </h1>
     <p
     className='text-lg  font-semibold text-indigo-600'
