7bb255c276d0c4a8b0b59e51605b3848e877e339
rachidfrex <2003043000250@ofppt-edu.ma>
2024-05-12 22:10:17 +0100

    Refactor App component to update background colors and improve styling

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ function App() {
   return (
     <div className=' relative w-full '>
       <div className=" fixed -top-10 -left-10 h-[20%] w-[60%] sm:w-[30%] -z-10   bg-violet-500   blur-[100px]  "/>
-      <div className=" absolute top-10 left-32 h-[20%] w-[60%]  sm:w-[30%] -z-10  bg-pink-500/40    blur-[150px] "/>
+      <div className="    absolute top-10 left-32 h-[20%] w-[60%]  sm:w-[30%] -z-10  bg-pink-500/40    blur-[150px] "/>
       <div className=" fixed top-0 left-0   grid-bg mix-blend-multiply -z-10  "/>
      
       
-     <div className='  relative w-full flex flex-col gap-2  items-center   m-0 h-fit'>
+     <div className='   relative w-full flex flex-col gap-2  items-center   m-0 h-fit'>
      <Nav/>
      </div>
      
