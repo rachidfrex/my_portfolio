7215d90ef117b48039864b8a461a81e3a7182844
rachidfrex <2003043000250@ofppt-edu.ma>
2024-05-12 19:31:05 +0100

    Refactor App component to update background colors and blur effect

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
 
   return (
     <div className='  relative flex flex-col justify-start items-center gap-2    w-full m-0 h-fit '>
-      <div className=" fixed -top-10 -left-10 h-96 w-96    bg-blue-500  blur-[200px] "/>
-      <div className=" absolute top-10 left-32 h-96 w-96   bg-pink-500/45   blur-[200px] "/>
+      <div className=" fixed -top-10 -left-10 h-96 w-96    bg-blue-500/30  blur-[200px] "/>
+      <div className=" absolute top-10 left-32 h-96 w-96   bg-teal-500/30   blur-[200px] "/>
       <div className=" fixed top-0 left-0   grid-bg mix-blend-multiply "/>
       {/* <Nab/> */}
       
