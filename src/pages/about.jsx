18e5646f349cb9f13e7a0f59043736ce4bacfa43
rachidfrex <danzu2009@gmail.com>
2024-06-24 21:35:45 -0400

    Refactor card component to include icon prop and improve code organization

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Linelaser from "../components/linelaser";
+
 import Cards from "../components/cards";
 
 
 const About = () => {
     return (
-       <div className='flex justify-center items-center gap-5 w-full bg-slate-950 ' >
-         <div className='flex justify-center items-center   '>
+       <div className='flex flex-col justify-center items-center gap-5 w-full bg-slate-950 ' >
+         <div className='flex    justify-center items-center   '>
          <div className='border-2 rounded-xl bg-gradient-to-t from-violet-800 to-violet-950 dark:from-violet-500 dark:to-violet-700 p-4 backdrop-filter backdrop-blur-lg bg-opacity-40'>
             <Linelaser/>
+           
         </div>
         </div>
       
