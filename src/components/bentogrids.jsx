93a1c5fc8f0dfc80afbc96dc1416c626f94ca782
rachidfrex <danzu2009@gmail.com>
2024-07-22 21:25:59 -0400

    refactor: Update Bentogrids component styles and structure for better responsiveness

--- src/components/bentogrids.jsx
+++ src/components/bentogrids.jsx
@@ -6,7 +6,7 @@ import logo from "../assets/logo.png"
 const Bentogrids = () => {
     return (
         <div
-            className='max-w-7xl  px-4 lg:mx-auto w-full h-screen flex justify-center   items-center'
+            className='max-w-[1550px]  px-4 lg:mx-auto w-full h-screen flex justify-center   items-center'
         >
             <div
                 className='grid grid-cols-4  grid-rows-6 lg:grid-rows-3 gap-4 h-full w-full'
@@ -19,8 +19,15 @@ const Bentogrids = () => {
                 <div className='bg-indigo-200  col-span-4  row-span-1 lg:col-span-2 lg:row-span-1 rounded-3xl flex justify-center items-center'>
                 colomn 2
                 </div>
-                <div className='bg-indigo-200 col-span-4  row-span-1 lg:col-span-1 lg:row-span-2 rounded-3xl flex justify-center items-center'>
-                    colomn 3
+                <div className='bg-violet-500 col-span-4  row-span-1 lg:col-span-1 lg:row-span-2 rounded-3xl flex  justify-center items-center'>
+                    <div className='flex-col justify-center  g items-center text-center'>
+                    <h3 className='text-3xl font-semibold '>
+                        Modern and <br /> Beautiful UI
+                    </h3>
+                    <p className='mt-5 text-lg'>
+                        Create stunning <br /> profesional-quality <br /> website
+                    </p>
+                    </div>
                 </div>
                 <div className= '  dark:bg-blue-900/80 relative overflow-hidden   backdrop-blur-lg text-white p-6  col-span-4  row-span-1 lg:col-span-2 lg:row-span-1 rounded-3xl flex justify-center items-center border-2 border-gray-700'>
                     <div class="absolute right-0 blur-sm top-0 z-59 h-full w-full  bg-transparent bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
